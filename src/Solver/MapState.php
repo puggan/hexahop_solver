a1176ed371e5fab4dd0d99bcdc4e18463051891d
Thorbjörn Sundragon <puggan@spiro.se>
2019-07-13 16:25:12 +0200

    MapState

---
+++ src/Solver/MapState.php
@@ -0,0 +1,102 @@
+<?php
+
+	namespace Puggan\Solver;
+
+	abstract class MapState
+	{
+		abstract public function __construct($data, $path = null);
+
+		/**
+		 * Player have won
+		 * @return bool
+		 */
+		abstract public function won() : bool;
+
+		/**
+		 * Player have lost
+		 * @return bool
+		 */
+		abstract public function lost() : bool;
+
+		/**
+		 * The game allows thus moves to be executed
+		 * May include moves that make the player lose
+		 * @return int[]
+		 */
+		abstract public function possible_moves() : array;
+
+		/**
+		 * Clones the state, make a move, and return that new state
+		 *
+		 * @param int $move move/direction to travel
+		 *
+		 * @return MapState Pure function, no side-effect allowed, so not $this
+		 */
+		public function move($move) : self
+		{
+			$state = clone $this;
+			$state->_move($move);
+			return $state;
+		}
+
+		/**
+		 * Make a move in the current state
+		 * @param int $move move/direction to travel
+		 */
+		abstract protected function _move($move) : void;
+
+		/**
+		 * Execute all posible moves, and return a list
+		 * @return MapState[]
+		 */
+		public function move_all() : array
+		{
+			/** @var MapState[] $states */
+			$states = [];
+			foreach($this->possible_moves() as $move)
+			{
+				$states[$move] = $this->move($move);
+			}
+			return $states;
+		}
+
+		/**
+		 * @return string uniq state hash, used to detect duplicates
+		 */
+		abstract public function hash() : string;
+
+		/**
+		 * @param int[] $path
+		 *
+		 * @return MapState
+		 */
+		public function path($path) : self
+		{
+			$state = clone $this;
+			foreach($path as $move)
+			{
+				$state->_move($move);
+			}
+			return $state;
+		}
+
+		/**
+		 * @param MapState $a
+		 * @param MapState $b
+		 *
+		 * @return int -1, 0, 1
+		 */
+		abstract public static function cmp($a, $b) : int;
+
+		/**
+		 * is the current state better that this other state?
+		 *
+		 * @param MapState $other
+		 *
+		 * @return bool
+		 */
+		public function better($other) : bool
+		{
+			return self::cmp($this, $other);
+		}
+	}
