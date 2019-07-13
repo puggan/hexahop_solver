1aeb7cf9c360089daac7c2f0f945f73ca53773ef
Thorbjörn Sundragon <puggan@spiro.se>
2019-07-13 21:12:47 +0200

    Help-classes for the IDE

---
+++ src/phpdoc.php
@@ -0,0 +1,33 @@
+<?php
+
+	namespace PHPDoc;
+
+	/**
+	 * Class MapInfo
+	 * @package PHPDoc
+	 *
+	 * @property int map_id
+	 * @property string file
+	 * @property string title
+	 * @property int level_number
+	 * @property int width
+	 * @property int height
+	 * @property int par
+	 * @property int start_x
+	 * @property int start_y
+	 */
+	class MapInfo
+	{
+	}
+
+	/**
+	 * Class Player
+	 * @package PHPDoc
+	 * @property boolean alive
+	 * @property int x
+	 * @property int y
+	 * @property int z
+	 */
+	class Player
+	{
+	}
