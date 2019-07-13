a048bde0899f1e40620bf4e615698f202e89fae4
Thorbjörn Sundragon <puggan@spiro.se>
2019-07-13 16:29:31 +0200

    TodoStorage

---
+++ src/Solver/TodoStorage.php
@@ -0,0 +1,30 @@
+<?php
+
+	namespace Puggan\Solver;
+
+	abstract class TodoStorage
+	{
+		/**
+		 * Adds a path to the todo
+		 * @param int[] $path
+		 */
+		abstract public function add($path): void;
+
+		/**
+		 * Reserve a todo, thats not already reserved
+		 * @param int $pid
+		 *
+		 * @return false|int[]
+		 */
+		abstract public function reserve($pid);
+
+		/**
+		 * @param int[] $path
+		 */
+		abstract public function remove($path): void;
+
+		/**
+		 * @param int[] $path
+		 */
+		abstract public function remove_all($path): void;
+	}
