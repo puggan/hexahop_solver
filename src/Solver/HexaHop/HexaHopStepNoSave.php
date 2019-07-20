c161dce4c8d56210bc92b910a7f48aa6461bfaae
Thorbjörn Sundragon <puggan@spiro.se>
2019-07-21 01:17:06 +0200

    NoSave solver improved, save stats and more

--- src/Solver/HexaHop/HexaHopStepNoSave.php
+++ src/Solver/HexaHop/HexaHopStepNoSave.php
@@ -8,7 +8,7 @@
 
 	define('DOT_STEPS', 1000);
 	define('DISPLAY_STEPS', 100000);
-	define('SLEEP_TIME', 100);
+	define('SLEEP_TIME', 0);
 
 	if($argc < 2)
 	{
@@ -20,10 +20,16 @@
 	require_once dirname(__DIR__, 3) . '/vendor/autoload.php';
 
 	$start_state = new HexaHopMap($level_number);
-	$solver = new SolverNoSave($start_state, new IniHashStorage(HexaHopSolver::data_dir($level_number) . 'solved.ini'));
+	$solved_filename = HexaHopSolver::data_dir($level_number) . 'solved.ini';
+	if(is_file($solved_filename))
+	{
+		unlink($solved_filename);
+	}
+	$solver = new SolverNoSave($start_state, new IniHashStorage($solved_filename));
 	echo $start_state->map_info(JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES), PHP_EOL;
 	$steps = 0;
 	$timestamp = hrtime(TRUE);
+	$start_time = $timestamp;
 	while($solver->step())
 	{
 		++$steps;
@@ -38,9 +44,56 @@
 			echo $steps, ' @ ', (DISPLAY_STEPS * 1e9 / ($timestamp - $old)), PHP_EOL;
 			echo json_encode($solver->debug()), PHP_EOL;
 		}
-		//usleep(SLEEP_TIME);
-		//sleep(1);
-		//echo json_encode($solver->debug()), PHP_EOL;
+		if(SLEEP_TIME > 0)
+		{
+			usleep(SLEEP_TIME);
+			//sleep(1);
+			//echo json_encode($solver->debug()), PHP_EOL;
+		}
 	}
+	$endtime = hrtime(TRUE);
 	echo PHP_EOL, 'Done!', PHP_EOL;
-	echo file_get_contents(dirname(__DIR__, 3) . '/data/' . $level_number . '/solved.ini'), PHP_EOL;
+	if(is_file($solved_filename))
+	{
+		echo file_get_contents($solved_filename), PHP_EOL;
+		echo 'Solved in ', number_format($steps, 0, '.', ' '), ' steps', PHP_EOL;
+		$full_time = ($endtime - $start_time)/1000000000;
+		echo 'Solved in ', $full_time, ' secounds', PHP_EOL;
+		if(SLEEP_TIME)
+		{
+			$sleep_time = SLEEP_TIME * $steps;
+			echo ' - Sleeping ', $sleep_time, ' secounds, (', 100 * $sleep_time / $full_time, '%)', PHP_EOL;
+			echo ' + Working ', ($full_time - $sleep_time), ' secounds, (', 100 - 100 * $sleep_time / $full_time, '%)', PHP_EOL;
+		}
+		if($full_time > 3600 * 24)
+		{
+			echo ' = ', $full_time / 3600 / 24, ' days', PHP_EOL;
+		}
+		else if($full_time > 3600)
+		{
+			echo ' = ', $full_time / 3600, ' hours', PHP_EOL;
+		}
+		else if($full_time > 60)
+		{
+			echo ' = ', $full_time / 60, ' minutes', PHP_EOL;
+		}
+		echo ' => ', $steps/$full_time, ' steps per secound', PHP_EOL;
+
+		$stat_filename = dirname(__DIR__, 3) . '/data/stats.json';
+		if(is_file($stat_filename))
+		{
+			$stats = (array) json_decode(file_get_contents($stat_filename), false);
+			$stats[$level_number]->steps = $steps;
+			$stats[$level_number]->time = $full_time;
+			file_put_contents($stat_filename, json_encode($stats, JSON_PRETTY_PRINT));
+		}
+
+		$full_solved_filename = dirname(__DIR__, 3) . '/data/solved/' . $level_number . '.ini';
+		rename($solved_filename, $full_solved_filename);
+		rmdir(dirname(__DIR__, 3) . '/data/' . $level_number);
+	}
+	else
+	{
+		echo 'Not solved :-(', PHP_EOL;
+		die(1);
+	}
