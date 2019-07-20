20f43d21691c556b816c71d7d2683ff78328c8a8
Thorbjörn Sundragon <puggan@spiro.se>
2019-07-20 12:34:16 +0200

    Script to fast test a path

---
+++ src/Solver/HexaHop/MapTest.php
@@ -0,0 +1,106 @@
+#!/usr/bin/env php
+<?php
+
+	use Puggan\Solver\HexaHop\HexaHopMap;
+
+	require_once dirname(__DIR__, 3) . '/vendor/autoload.php';
+
+	$id = $argv[1] ?? NULL;
+	$path_str = $argv[2] ?? NULL;
+
+	if(!$id)
+	{
+		echo 'No ID';
+		die(1);
+	}
+
+	/** @var \PHPDoc\MapInfo[] $maps */
+	$maps = HexaHopMap::list_maps();
+
+	if(empty($maps[$id]->title))
+	{
+		echo 'Bad map, no title';
+		die(1);
+	}
+
+	/** @noinspection NonSecureHtmlentitiesUsageInspection */
+	$map_info = (object) array_map('htmlentities', (array) $maps[$id]);
+	$map = new HexaHopMap($id);
+
+	if(!$map)
+	{
+		echo 'Bad map';
+		die(1);
+	}
+
+	$path = [];
+	if($path_str || $path_str === '0')
+	{
+		if(is_array($path_str))
+		{
+			$path = array_map('intval', $path_str);
+		}
+		else if(is_string($path_str))
+		{
+			$path = array_map('intval', explode(',', $path_str));
+		}
+	}
+
+	$alive = true;
+
+	if($path)
+	{
+		$directions = ['North', 'Nort-East', 'South-East', 'South', ' South-West', 'North-West', 'Jump'];
+		foreach(array_values($path) as $index => $dir)
+		{
+			if(empty($directions[$dir]))
+			{
+				break;
+			}
+
+			/** @var HexaHopMap $map */
+			$map = $map->move($dir);
+
+			if($map->won())
+			{
+				echo 'Step: ', ($index + 1), ', Points: ', $map->points(), ' / ', $map->par(), PHP_EOL;
+				echo 'Won', PHP_EOL;
+				$alive = true;
+				break;
+			}
+			if($map->lost())
+			{
+				echo 'Step: ', ($index + 1), ', Points: ', $map->points(), ' / ', $map->par(), PHP_EOL;
+				echo 'Lost', PHP_EOL;
+				$alive = false;
+				break;
+			}
+			if($map->imposible())
+			{
+				echo 'Step: ', ($index + 1), ', Points: ', $map->points(), ' / ', $map->par(), PHP_EOL;
+				echo 'Imposible', PHP_EOL;
+				$alive = false;
+				break;
+			}
+		}
+	}
+
+	if($alive)
+	{
+		if(!$map->won())
+		{
+			echo 'Step: ', ($index + 1), ', Points: ', $map->points(), ' / ', $map->par(), PHP_EOL;
+			echo 'Still alive', PHP_EOL;
+			die(-2);
+		}
+
+		if($map->points() < $map->par())
+		{
+			echo 'Beat Par', PHP_EOL;
+			die(1);
+		}
+	}
+	else
+	{
+		die(-1);
+	}
