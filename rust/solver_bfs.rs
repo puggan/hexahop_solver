120ab85d30e8c7e3719b8460084fca8879ecbe16
Thorbjörn Puggan Sundragon <puggan@sundragon.se>
2026-05-17 14:49:14 +0200

    Make a struct of run_solver's local vars

--- rust/solver_bfs.rs
+++ rust/solver_bfs.rs
@@ -12,6 +12,7 @@ use crate::map::MapState;
 use crate::map::MapStatus;
 use crate::map;
 use crate::step::GameState;
+use crate::step::GameStateGhost;
 #[cfg(feature = "h128")]
 use std::hash::Hash;
 #[cfg(feature = "h128")]
@@ -32,66 +33,91 @@ fn hash_map_state(mut hasher: &mut Xxh3, state: &MapState) -> u128 {
     hasher.digest128()
 }
 
+struct Solver {
+    done: HashSet<u64>,
+    last_print_time: Instant,
+    last_todo_len: usize,
+    max_cost_or_par: u16,
+    pid: sysinfo::Pid,
+    start_time: Instant,
+    sys: System,
+    todo: BinaryHeap<GameStateGhost>,
+    won: Vec<GameStateGhost>,
+}
+
+impl Solver {
+    fn new(max_cost_or_par: u16, pid: sysinfo::Pid) -> Solver {
+        let now = Instant::now();
+        Solver {
+            done: HashSet::with_capacity(8_000_000),
+            last_print_time: now,
+            last_todo_len: 0,
+            max_cost_or_par,
+            pid,
+            start_time: now,
+            sys: System::new(),
+            todo: BinaryHeap::with_capacity(8_000_000),
+            won: Vec::new(),
+        }
+    }
+}
+
 // Breadth-First Search (BFS)
 pub fn run_solver(map_nr: usize, max_cost: &Option<u16>) -> Result<GameState, String> {
     let info = map::get(map_nr)?;
-    let mut todo = BinaryHeap::with_capacity(8_000_000);
-    let mut won = Vec::new();
-    let mut done = HashSet::with_capacity(8_000_000);
-    let max_cost_or_par = max_cost.unwrap_or(info.par);
-    let mut sys = System::new();
-    let pid = sysinfo::get_current_pid()?;
-    let mut last_print_time = Instant::now();
-    let mut last_todo_len = 0;
+    let mut solver = Solver::new(
+        max_cost.unwrap_or(info.par),
+        sysinfo::get_current_pid()?
+    );
     let start_state = GameState::new(MapState::load_lev(&info)?);
-    todo.push(start_state.ghost());
+    solver.todo.push(start_state.ghost());
     #[cfg(feature = "h128")]
     let mut hasher = Xxh3::new();
 
-    while todo.len() > 0 {
-        let game = todo.pop().unwrap().reproduce(start_state.clone(), &info);
+    while solver.todo.len() > 0 {
+        let game = solver.todo.pop().unwrap().reproduce(start_state.clone(), &info);
         dlog!("Testing game, score: {}, path: {}", game.cost, Direction::list2string(&game.path));
         #[cfg(not(feature = "h128"))]
         let hash = fxhash::hash64(&game.state);
         #[cfg(feature = "h128")]
         let hash = hash_map_state(&mut hasher, &game.state);
-        if done.contains(&hash) {
+        if solver.done.contains(&hash) {
             dlog!("Duplicate");
             continue;
         }
-        done.insert(hash);
+        solver.done.insert(hash);
 
-        if done.len() % 10000 == 0 {
-            sys.refresh_processes(ProcessesToUpdate::Some(&[pid]), true);
+        if solver.done.len() % 10000 == 0 {
+            solver.sys.refresh_processes(ProcessesToUpdate::Some(&[solver.pid]), true);
             let now = Instant::now();
-            let todo_len = todo.len();
-            let duration_float = now.duration_since(last_print_time).as_secs_f64();
+            let todo_len = solver.todo.len();
+            let duration_float = now.duration_since(solver.last_print_time).as_secs_f64();
             println!(
                 "{}: {:>2} | {}: {:>11} ({:>7}) | {}: {:>11} | {}: {:>3} {} | {}: {:>9}.{:02} | {}: {:>5.2} GiB",
                 "Won".yellow(),
-                won.len().to_string().color( if won.is_empty() { Color::Blue } else { Color::Green } ),
+                solver.won.len().to_string().color( if solver.won.is_empty() { Color::Blue } else { Color::Green } ),
                 "Queued".yellow(),
                 todo_len.to_formatted_string(&Locale::sv),
-                if todo_len < last_todo_len {
-                    format!("-{}", (last_todo_len - todo_len).to_formatted_string(&Locale::sv)).cyan()
-                } else if todo_len - last_todo_len > 10_000 {
-                    format!("+{}", (todo_len - last_todo_len).to_formatted_string(&Locale::sv)).red()
+                if todo_len < solver.last_todo_len {
+                    format!("-{}", (solver.last_todo_len - todo_len).to_formatted_string(&Locale::sv)).cyan()
+                } else if todo_len - solver.last_todo_len > 10_000 {
+                    format!("+{}", (todo_len - solver.last_todo_len).to_formatted_string(&Locale::sv)).red()
                 } else {
-                    format!("+{}", (todo_len - last_todo_len).to_formatted_string(&Locale::sv)).green()
+                    format!("+{}", (todo_len - solver.last_todo_len).to_formatted_string(&Locale::sv)).green()
                 },
                 "Done".yellow(),
-                done.len().to_formatted_string(&Locale::sv),
+                solver.done.len().to_formatted_string(&Locale::sv),
                 "Cost".yellow(),
                 game.cost,
-                format!("/ {}", max_cost_or_par).bright_black(),
+                format!("/ {}", solver.max_cost_or_par).bright_black(),
                 "Speed".yellow(),
                 ((1e4 / duration_float) as u128).to_formatted_string(&Locale::sv),
                 (1e6 / duration_float) as u128 % 100,
                 "Memory".yellow(),
-                sys.process(pid).map(|p| p.memory() as f64).unwrap_or(f64::NAN) / (1 << 30) as f64
+                solver.sys.process(solver.pid).map(|p| p.memory() as f64).unwrap_or(f64::NAN) / (1 << 30) as f64
             );
-            last_todo_len = todo_len;
-            last_print_time = now;
+            solver.last_todo_len = todo_len;
+            solver.last_print_time = now;
         }
 
         let directions: &[Direction] = if game.state.jumps > 0 {
@@ -108,8 +134,8 @@ pub fn run_solver(map_nr: usize, max_cost: &Option<u16>) -> Result<GameState, St
                     let hash = fxhash::hash64(&next_state.state);
                     #[cfg(feature = "h128")]
                     let hash = hash_map_state(&mut hasher, &next_state.state);
-                    done.insert(hash);
-                    won.push(next_state.ghost());
+                    solver.done.insert(hash);
+                    solver.won.push(next_state.ghost());
                 }
                 MapStatus::Dead => {
                     dlog!("dir {} Dead!", dir);
@@ -120,16 +146,16 @@ pub fn run_solver(map_nr: usize, max_cost: &Option<u16>) -> Result<GameState, St
                     let hash = fxhash::hash64(&next_state.state);
                     #[cfg(feature = "h128")]
                     let hash = hash_map_state(&mut hasher, &next_state.state);
-                    if !done.contains(&hash) {
-                        todo.push(next_state.ghost());
+                    if !solver.done.contains(&hash) {
+                        solver.todo.push(next_state.ghost());
                     }
                 }
             }
         }
     }
 
-    println!("won: {}, queued: {}, done: {}", won.len(), todo.len(), done.len());
-    let best = won.into_iter().max().ok_or_else(|| "No solution found".to_string())?.reproduce(start_state.clone(), &info);
+    println!("won: {}, queued: {}, done: {}", solver.won.len(), solver.todo.len(), solver.done.len());
+    let best = solver.won.into_iter().max().ok_or_else(|| "No solution found".to_string())?.reproduce(start_state.clone(), &info);
     println!("Best game, score: {}, path: {}", best.cost, Direction::list2string(&best.path));
     Ok(best.clone())
 }
